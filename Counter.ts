c4d0d08feee42a75302554cc0e9c2ae44d1ce0fc
Kartagia <antti@kautiainen.com>
2026-01-08 09:37:10 +0200

    fix: counter parsing value
    Introducing the fix on counter prasing the input on the counter
    input component.
    
    Changes to be committed:
    	modified:   Counter.ts
        - fix: parser for parsed value did not work.
        - fix: invalid calculation of at least min throwing SyntaxError.
        - fix: invalid calculation of at least max throwing SyntaxError.
        - fix: msising set parsed on confirm handler.
        - feat: use logger for grouping.

--- Counter.ts
+++ Counter.ts
@@ -1,7 +1,7 @@
 import { html, useEffect } from 'preact';
 import { useParsedValue, Parser, Format } from "./hooks";
-import { type Compare, defaultCompare } from "././utils";
-import {defaultConsoleLogger} from "./log";
+import { type Compare, defaultCompare } from "./utils";
+import { defaultConsoleLogger } from "./log";
 
 const log = defaultConsoleLogger();
 
@@ -10,38 +10,38 @@ const log = defaultConsoleLogger();
  * 
  * @template T The counted value type.
  */
-export interface CounterProps < T > {
+export interface CounterProps<T> {
   /**
    * The initial value of the component.
    */
   defaultValue: T;
-  
+
   /**
    * The formatter formatting the value into input component value.
    */
-  format: Format < T > ;
-  
+  format: Format<T>;
+
   /**
    * The parse of the value from input component value. If undefined, the value is not editable.
    */
-  parse ? : Parser < T > ;
-  
-  increase(source: T, amount ? : T): T;
-  
-  decrease(source: T, amount ? : T): T;
-  compare ? : Compare < T > ;
-  min ? : T;
-  max ? : T;
+  parse?: Parser<T>;
+
+  increase(source: T, amount?: T): T;
+
+  decrease(source: T, amount?: T): T;
+  compare?: Compare<T>;
+  min?: T;
+  max?: T;
 }
 
 /**
  * A simple counter component.
  */
 export function Counter() {
-  const [input, parsed, partial, setInput, setParsed] = useParsedValue < number > ({
+  const [input, parsed, partial, setInput, setParsed] = useParsedValue<number>({
     parsed: 0,
     format: (a: number) => a.toString(),
-    parse: (a: string) => Number.parseInt(a)
+    parser: (a: string) => Number.parseInt(a)
   });
   const decrease = () => {
     setParsed(parsed - 1)
@@ -50,49 +50,50 @@ export function Counter() {
     setParsed(parsed + 1)
   };
   return html`<div class="${partial ? "bold" : ""}"><span onclick="${decrease}">-</span>${parsed}<span onclick="${increase}">+</span></div>`;
-  
+
 }
 
-export function CounterControl < T > (props: Omit < CounterProps < T > , "defaultValue" > & {
+export function CounterControl<T>(props: Omit<CounterProps<T>, "defaultValue"> & {
   value: T;
-  action ? : EventHandlerNonNull < MouseEvent > ;
+  action?: EventHandlerNonNull<MouseEvent>;
   title: string;
 }) {
   const classNames = ["control"];
-  const compare: Compare < T > = props.compare ?? defaultCompare;
+  const compare: Compare<T> = props.compare ?? ((compared: T, comparee: T) => defaultCompare(compared, comparee));
   console.table({
     value: props.value,
-    min: props.min,max:props.max,
-    atLeastMin: (props.min === undefined || compare(props.min, props.value) <= 0),
-    atMostMax: (props.max === undefined || compare(props.value, props.max) <= 0)
+    min: props.min, max: props.max,
+    atLeastMin: props.min === undefined || (compare(props.min, props.value) <= 0),
+    atMostMax: props.max === undefined || (compare(props.value, props.max) <= 0)
   })
   if (
-    ("min" in props && compare(props.min, props.value) > 0) ||
-    ("max" in props && compare(props.value, props.max) > 0)
+    ("min" in props && props.min !== undefined && compare(props.min, props.value) > 0) ||
+    ("max" in props && props.max !== undefined && compare(props.value, props.max) > 0)
   ) {
     classNames.push("disabled");
   }
-  
+
   const handleClick = props.action;
   return html`<span class="${classNames.join(" ")}"  onClick="${handleClick}">${props.title}</span>`;
 }
 
-export function InputCounter < T > (props: CounterProps < T > ) {
-  const [input, parsed, partial, setInput, setParsed] = useParsedValue < T > ({
-    parsed: props.defaultValue,
-    format: props.format,
-    parse: props.parse
-  });
+export function InputCounter<T>(props: CounterProps<T>) {
   const format = props.format ?? ((s) => ("" + s));
+  const parser = props.parse ?? ((val: string) => { throw new Error("Parse not supported"); });
+  const [input, parsed, partial, setInput, setParsed] = useParsedValue<T>({
+    parsed: props.defaultValue,
+    format,
+    parser
+  });
   const compare: Compare<T> = props.compare ?? defaultCompare;
-  console.group("InputCounter");
+  log.group("InputCounter");
   console.table(props);
   console.table({
     input, parsed, partial
   });
   useEffect(
-    ()=>{
-      
+    () => {
+
     },
     [input]
   )
@@ -102,32 +103,37 @@ export function InputCounter < T > (props: CounterProps < T > ) {
   } else {
     inputClasses.push("success");
   }
-  
+
   const increase = () => {
     if (props.max === undefined || compare(parsed, props.max) < 0) {
-    console.log("Increasing %s", format(parsed));
-    setParsed(props.increase(parsed));
+      log.log("Increasing %s", format(parsed));
+      setParsed(props.increase(parsed));
     }
   };
   const decrease = () => {
     if (props.min === undefined || compare(parsed, props.min) > 0) {
-    console.log("Decreasing %s", format(parsed));
-    setParsed(props.decrease(parsed))
+      log.log("Decreasing %s", format(parsed));
+      setParsed(props.decrease(parsed))
     }
   };
-  
+
   const altered = (e) => {
     if (typeof e === "object" && "target" in e && typeof e === "object") {
-    log.info("Value changed to %s", e.target.value);
-    setInput(e.target.value);
+      log.info("Value changed to %s", e.target.value);
+      setInput(e.target.value);
     } else {
       log.info("input value => %s", e.target.value);
-}
+    }
   }
-  
+
   const confirmed = (e) => {
     if (typeof e === "object") {
-    log.info("Confirmed %s", e.target.value);
+      log.info("Confirmed %s", e.target.value);
+      try {
+        setParsed(parser(e.target.value))
+      } catch (error) {
+        setParsed(parsed);
+      }
     }
   };
   const inputComponent = (props.parse ?
@@ -138,6 +144,6 @@ export function InputCounter < T > (props: CounterProps < T > ) {
   );
   const decreaseControl = html`<${CounterControl} value="${parsed}" min="${props.min}" max="${props.max}" value="${parsed}" compare="${props.compare}" action="${decrease}" title="-" />`;
   const increaseControl = html`<${CounterControl} value="${parsed}" min="${props.min}" max="${props.max}" value="${parsed}" compare="${props.compare}" action="${increase}" title="+" />`;
-  console.groupEnd();
+  log.groupEnd();
   return html`<div class="${partial ? "bold" : ""}">${decreaseControl}${inputComponent}${increaseControl}</div>`;
 }
