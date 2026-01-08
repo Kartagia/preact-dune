3c7d0b730f089587aa088a71a911983c5f57ccbb
Kartagia <antti@kautiainen.com>
2026-01-08 08:50:28 +0200

    fix: errors in logger
    Changes to be committed:
    	modified:   log.ts
        - fix: The log level types must extend string, number or symbol.
        - fix: class Logger generic parameter to log level type.
        - fix: Logger output does generate the log level and the timestamp
        to the output.
        - feat: type LogLevelOrDefault<LEVEL>
        - feat: class GroupingLogger<LEVEL as log level> extends Logger<LEVEL>
            Implements logger with groups.
            - feat: group(title: string) to start or activate a group.
            - feat: group() to start an anonymous group.
            - feat: groupEnd() to close the currently active group.
            - feat: groupEnd(title: string) to close the group with title.
        - feat: defautlConsoleLogger implements GroupingLogger instead of Logger.
        - doc: type LogFunction
        - doc: type LogFormat
        - doc: type LogLevel
        - doc: constant DEFAULT_LOG_LEVEL
        - doc: constant LogLevels
        - doc:

--- log.ts
+++ log.ts
@@ -1,17 +1,47 @@
 
-import {format} from "./format"
+import { format } from "./format"
 
+/**
+ * A function outputting a log message.
+ * 
+ * @param template The template of the message.
+ * @param args The template arguments. 
+ */
 export type LogFunction = (template: string, ...args: any[]) => void;
 
+/**
+ * The log function formatting a template into log string.
+ * 
+ * @param template The template of the message.
+ * @param args The template arguments. 
+ * @returns The string containing the formatted string.
+ */
 export type LogFormat = (template: string, ...args: any[]) => string;
 
+/**
+ * The tyep of default log levels taken from the Console. 
+ */
 export type LogLevel = "trace" | "debug" | "warn" | "info" | "error" | "fatal";
 
+/**
+ * The default log level.
+ */
 export const DEFAULT_LOG_LEVEL: LogLevel = "info";
 
-export const LogLevels: ReadonlyArray < LogLevel > = Object.freeze(["trace", "debug", "warn", "info", "error", "fatal"]);
+/**
+ * Log levels conntains list of the default log levels.
+ */
+export const LogLevels: ReadonlyArray<LogLevel> = Object.freeze(["trace", "debug", "warn", "info", "error", "fatal"]);
 
-export class Logger < LEVEL = LogLevels > {
+/**
+ * Log level or default.
+ */
+export type LogLevelOrDefault<LEVEL extends (string | number | symbol)> = LEVEL | "default";
+
+/**
+ * The logger class is a logging interface similar to the console.
+ */
+export class Logger<LEVEL extends string | number | symbol = LogLevel> {
   static log(template: string, ...args: any[]) {
     console.info('[%s]: ' + template, (new Date()).toISOString(), ...args)
   }
@@ -21,30 +51,30 @@ export class Logger < LEVEL = LogLevels > {
   static error(template: string, ...args: any[]) {
     console.error('[%s]: ' + template, (new Date()).toISOString(), ...args);
   }
-  
-  readonly levels: Readonly < Record < LogLevel | "default",
-  LEVEL >> ;
-  
-  readonly streams: Readonly<Record<LEVEL|"default", LogFunction>>;
-  
+
+  readonly levels: Readonly<Record<LogLevelOrDefault<LogLevel>, LogLevelOrDefault<LEVEL>>>;
+
+  readonly streams: Readonly<Record<LogLevelOrDefault<LEVEL>, LogFunction>>;
+
   constructor(
-    levelMap: Readonly < Record < LogLevel | "default", LEVEL >>,
-    streams: Readonly<Record<LEVEL|"default", LogFunction>> = {default: console.log}) {
-    this.levels = Object.freeze({ ...levelMap, default: levelMap.default ?? levelMap[DEFAULT_LOG_LEVEL] });
+    levelMap: Readonly<Record<LogLevelOrDefault<LogLevel>, LogLevelOrDefault<LEVEL>>>,
+    streams: Readonly<Record<LogLevelOrDefault<LEVEL>, LogFunction>> = { default: console.log } as Readonly<Record<LogLevelOrDefault<LEVEL>, LogFunction>>
+  ) {
+    this.levels = Object.freeze({ ...levelMap, default: (levelMap.default) });
     this.streams = streams;
   }
-  
-  levelStream(level: LEVEL): (
+
+  levelStream(level: LogLevelOrDefault<LEVEL>): (
     undefined | LogFunction
   ) {
     return this.streams[level] ?? this.streams.default;
   }
-  
-  add(level: LEVEL, template: string, ...args: any[]) {
+
+  add(level: LogLevelOrDefault<LEVEL>, template: string, ...args: any[]) {
     const out = this.levelStream(level);
-    out?.log("[%s][%s]: " + template, ...args);
+    out?.("[%s][%s]: " + template, (new Date()).toISOString(), level, ...args);
   }
-  
+
   error(template: string, ...args: any[]) {
     const level = this.levels.error;
     if (level) this.add(level, template, ...args);
@@ -65,10 +95,6 @@ export class Logger < LEVEL = LogLevels > {
     const level = this.levels.debug;
     if (level) this.add(level, template, ...args);
   }
-  info(template: string, ...args: any[]) {
-    const level = this.levels.info;
-    if (level) this.add(level, template, ...args);
-  }
   fatal(template: string, ...args: any[]) {
     const level = this.levels.fatal;
     if (level) this.add(level, template, ...args);
@@ -79,37 +105,85 @@ export class Logger < LEVEL = LogLevels > {
   }
 }
 
-export function defaultConsoleLogger(): Logger {
-  return new Logger(
+export class GroupingLogger<LEVEL extends string|number|symbol = LogLevel> extends Logger<LEVEL> {
+  protected groupFunctions: { open: (title: string, collapsed: boolean) => void; close: (title: string) => void; };
+
+  constructor(
+    levelMap: Readonly<Record<LogLevelOrDefault<LogLevel>, LogLevelOrDefault<LEVEL>>>,
+    groupFunctions: { open: (title: string, collapsed: boolean) => void, close: (title: string) => void },
+    streams: Readonly<Record<LogLevelOrDefault<LEVEL>, LogFunction>> = { default: console.log } as Readonly<Record<LogLevelOrDefault<LEVEL>, LogFunction>>
+  ) {
+    super(levelMap, streams);
+    this.groupFunctions = groupFunctions;
+  }
+
+  group(): void;
+  group(template: string, ...args: string[]): void;
+  group(template?: string, ...args: string[]): void {
+    if (arguments.length === 0 || template === undefined) {
+      this.groupFunctions.open(template ?? "", false);
+    } else {
+      const title = this.groupFunctions.open(format(template, ...args), false);
+    }
+  }
+
+  groupEnd(): void;
+  groupEnd(title: string): void;
+  groupEnd(title?: string): void {
+    if (arguments.length === 0 || title === undefined) {
+      this.groupFunctions.close(title ?? "");
+    } else {
+      this.groupFunctions.close(title);
+    }
+  }
+}
+
+
+export function defaultConsoleLogger<LEVEL extends (string|number|symbol) = LogLevel>(): GroupingLogger<LEVEL> {
+  return new GroupingLogger(
     LogLevels.reduce(
-        (res, level) => ({...res,
-          [level]: level}), {default: DEFAULT_LOG_LEVEL}
-      ),
+      (res: Readonly<Record<LogLevelOrDefault<LogLevel>, LogLevelOrDefault<LEVEL>>>, level: LogLevelOrDefault<LogLevel>) => ({
+        ...res,
+        [level]: level
+      }),
+      { default: DEFAULT_LOG_LEVEL } as Readonly<Record<LogLevelOrDefault<LogLevel>, LogLevelOrDefault<LEVEL>>>
+    ),
+    {
+      open: (title: string, collapsed) => collapsed ? console.groupCollapsed(title) : console.group(title),
+      close: console.groupEnd
+    },
     LogLevels.reduce(
-        (res, level) => ({...res,
-          [level]: console[level]
-        }), {default: console.log}
-      )
+      (res: Readonly<Record<LogLevelOrDefault<LEVEL>, LogFunction>>, level: LogLevelOrDefault<LogLevel>) => ({
+        ...res,
+        ...(level in console ? { [level]: (level != "default" && level in console ? (console[level] as LogFunction) : console.info) } : {})
+      }), { default: console.log } as Readonly<Record<LogLevelOrDefault<LEVEL>, LogFunction>>
+    )
   );
 }
 
-export class FormatLog<LEVEL=LogLevel>
-extends Logger<LEVEL> {
+export class FormatLog<LEVEL extends (string|number|symbol)= LogLevel>
+  extends Logger<LEVEL> {
+
+  /**
+   * The formatter of the logger.
+   */
+  format: LogFormat;
+
   constructor(
-    levelMap: Readonly < Record < LogLevel | "default", LEVEL >>,
+    levelMap: Readonly<Record<LogLevel | "default", LEVEL>>,
     format: LogFormat
   ) {
     super(levelMap);
     this.format = format;
   }
-  
+
   levelStream(level: LEVEL): LogFunction {
     return (template, ...args) => {
       super.levelStream(level)?.("%s", this.format(template, ...args));
     }
   }
-  
+
   add(level: LEVEL, template: string, ...args: any[]) {
-    
+
   }
 }
