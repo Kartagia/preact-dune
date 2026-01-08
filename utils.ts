1087c1de4c4888514e627a84ab5822474f7505b8
Kartagia <antti@kautiainen.com>
2026-01-08 08:45:50 +0200

    fix-feat-doc:
    Changes to be committed:
    	modified:   utils.ts
        - fix: function atTitle return empty string as it is.
        - feat: type ComparisonResult
        - feat: type Compare<T>
        - doc: type Compare<T>
        - feat: function defaultCompare<T>(a: T, b: T)
        - feat: function defaultCompare<T>(a: T, b: T, equality: Equality<T>)
        - doc: function strictCompare
        - doc: function defaultEquality
        - doc: function sameValueZeroCompare
        - doc: function sameValueCompare

--- utils.ts
+++ utils.ts
@@ -1,18 +1,18 @@
 
-export interface Equality<T=any> {
+export interface Equality<T = any> {
   /**
    * @param a The compared value
    * @param b The value compared to.
    * @returns True, if and only if a is equal to b.
    */
-  (a: T, b:T):boolean
+  (a: T, b: T): boolean
 }
 
 /**
  * Loose equality.
  * @type {Equality<T>}
  */
-export function looseEquality<T>(a:T, b:T) {
+export function looseEquality<T>(a: T, b: T) {
   return a == b;
 }
 
@@ -20,7 +20,7 @@ export function looseEquality<T>(a:T, b:T) {
  * Strict equality.
  * @type {Equality<T>}
  */
-export function strictEquality<T>(a:T, b:T) {
+export function strictEquality<T>(a: T, b: T) {
   return a === b;
 }
 
@@ -28,50 +28,112 @@ export function strictEquality<T>(a:T, b:T) {
  * Same value equality.
  * @type {Equality<T>}
  */
-export function sameValueEquality<T>(a:T, b:T) {
-  return Object.is(a,b);
+export function sameValueEquality<T>(a: T, b: T) {
+  return Object.is(a, b);
 }
+
 /**
  * Same value zero equality.
  * @type {Equality<T>}
  */
-export function sameValueZeroEquality<T>(a:T, b:T) {
-  return a === b || (typeof a === typeof b && typeof a === "number" && (a !== a) && (b !== b) )
+export function sameValueZeroEquality<T>(a: T, b: T) {
+  return a === b || (typeof a === typeof b && typeof a === "number" && (a !== a) && (b !== b))
 }
 
 
-export function orElse<T>(value: T?, defaultValue:T, nullValue:T=defaultValue):T {
+export function orElse<T>(value: T | undefined | null, defaultValue: T, nullValue: T = defaultValue): T {
   if (value === null) {
     return nullValue;
-  } 
+  }
   if (value === undefined) {
     return defaultValue;
   }
   return value;
 }
 
+/**
+ * A comparison result.
+ */
+export type ComparisonResult = -1 | 0 | 1;
+
+/**
+ * Compare two values
+ * 
+ * @param compared The compared value.
+ * @param comparee The value compared with.
+ * @returns The signum of the comparison result: -1 for compared less than comparee, 0 for compared equla comparee, and 1 for compared
+ * greater than comparee.
+ * @throws {SyntaxError} The values are not comparable.
+ */
+export type Compare<T> = (compared: T, comparee: T) => ComparisonResult;
+
+/**
+ * Compare values using loose equality.
+ * @param a The compared value.
+ * @param b The value compared with.
+ * @returns The comparison result.
+ */
+export function defaultCompare<T>(a: T, b: T): ComparisonResult;
+
+/**
+ * Compare values using an equality.
+ * @param a The compared value.
+ * @param b The value compared with.
+ * @param equals The equality used.
+ * @returns The comparison result.
+ */
+export function defaultCompare<T>(a: T, b: T, equals: Equality<T>): ComparisonResult;
 export function defaultCompare<T>(a: T, b: T, equals: Equality<T> = looseEquality
-): number {
-  return equals(a,b) ? 0 : a < b ? -1 : (a > b ? 1 : Number.NaN )
+): ComparisonResult {
+  if (equals(a, b)) return 0;
+  if (a < b) return -1;
+  if (a > b) return 1;
+  throw new SyntaxError("Values are not comparable");
 }
 
-export function sameValueZeroCompare<T>(a: T, b: T) {
-  return defaultCompare(a,b, sameValueZeroEquality)
+/**
+ * Compare values using same value zero equality.
+ * @param a The compared value.
+ * @param b The value compared with.
+ * @returns The comparison result.
+ */
+export function sameValueZeroCompare<T>(a: T, b: T): ComparisonResult | number {
+  return defaultCompare(a, b, sameValueZeroEquality)
 }
 
-export function sameValueCompare<T>(a: T, b: T) {
-  return defaultCompare(a,b, sameValueEquality)
+/**
+ * Compare values using same value equality.
+ * @param a The compared value.
+ * @param b The value compared with.
+ * @returns The comparison result.
+ */
+export function sameValueCompare<T>(a: T, b: T): ComparisonResult | number {
+  return defaultCompare(a, b, sameValueEquality)
 }
+
+/**
+ * Compare values using strict equality.
+ * @param a The compared value.
+ * @param b The value compared with.
+ * @returns The comparison result.
+ */
+export function strictValueCompare<T>(a: T, b: T): ComparisonResult | number {
+  return defaultCompare(a, b, strictEquality)
+}
+
 
 /**
  * Convert a string into title case.
  * @param text The converted text.
  * @returns The text with first glyph in upper case.
  */
-export function asTitle(text: string):string {
+export function asTitle(text: string): string {
   console.group(`asTitle(${text})`);
   try {
     const first = text.codePointAt(0);
+    if (first == null) {
+      return text;
+    }
     const firstChar = String.fromCodePoint(first);
     return firstChar.toLocaleUpperCase() + text.substring(firstChar.length);
   } finally {
