590e9ddada9e2a37912efe9285c59454a502521e
Kartagia <antti@kautiainen.com>
2026-02-16 05:13:10 +0200

    feat: settings test app
    Introducing settings testing component SettingsApp.
    
    Changes to be committed:
    	new file:   settings.test.ts
        - feat: settings testing component SettingsApp

---
+++ settings.test.ts
@@ -0,0 +1,32 @@
+import {html} from "preact";
+import {SettingsItem, type SettingsItemProps} from "./settings.item";
+import { SettingItemModel, SettingsCategoryModel } from "./settings.model";
+import {SettingsCategory } from "./settings.category";
+
+export function SettingsApp() {
+  const setting : SettingItemModel = {
+    name:"test",
+    label: "Test value"
+  };
+  const category: SettingsCategoryModel = {
+    title: "Test Category",
+    entries: [setting]
+  }
+  try {
+    const handleChange = (prop, value) => {
+      alert(`Set ${prop} to ${value}`)
+    }
+  console.table(setting)
+  console.log(typeof SettingsItem)
+;
+const field = html`<${SettingsItem} model="${setting}" onchange="${handleChange}" />`;
+return html`<div>
+  <h4>Settings</h4>
+  <${SettingsCategory} 
+  model="${category}"
+  />
+  </div>`
+  } finally {
+    console.log("Setting rendered")
+  }
+}
