fa44577dd91e5d0f29376fbcfcc4cfdb10a2c19f
Kartagia <antti@kautiainen.com>
2026-02-17 09:11:55 +0200

    feat: SettingsController
    Introducing a settings controller handling settings change mitigation
    to the settings models, and the value of the settings.
    
    Introducing method update to update the controlled value by the proposed
    value change. The method allows defining the new source value of the
    controller content. If source content is altered, it replaces the current
    controller content. Otherwise the source content is returned.
    
    Introducing methods setItemValue and setCategoryValue to encode the
    content value into a SettingItemModel or into a SettingCategoryModel.

---
+++ settings.control.ts
@@ -0,0 +1,45 @@
+import { SettingCategoryModel, SettingItemModel } from "./settings.model";
+
+/**
+ * The controller of settings.
+ */
+export interface SettingsController<T> {
+    _new?: (newContent: T) => T;
+    /**
+     * The keys of the default content.
+     */
+    keys: Array<keyof T>;
+    /**
+     * THe default content.
+     */
+    content: T;
+    /**
+     * Update the value at property path, and return the new content.
+     * @param propPath The property path.
+     * @param value The new value as string.
+     * @param current The current settings. Default to the current content.
+     * @returns The new content, if the update is valid. Returns the current content, if the
+     * value update was invalid.
+     */
+    update(propPath: string[], value?: string | undefined, current?: T): T;
+
+    /**
+     * Update the setting item model by assigning the stringification of the given property value as the
+     * value of the source model, and returning the created new instance.
+     * @param source The source setting item model.
+     * @param property The property,whose value is assigned to the setting item mode.
+     * @returns If the controller has value for the property, a new setting item model with value replaced with the value of the
+     * setting property. Otherwise the original source is returned.
+     */
+    setItemValue(source: SettingItemModel, property: string): SettingItemModel;
+
+    /**
+     * Update the setting category model by assigning the stringification of the given property value as the
+     * value of the source model, and returning the created new instance.
+     * @param source The source setting category model.
+     * @param property The property, whose value is assigned to the setting item mode.
+     * @returns If the controller has value for the property, a new setting category model with value replaced with the value of the
+     * setting property. Otherwise the original source is returned.
+     */
+    setCategoryValue(source: SettingCategoryModel, property: string): SettingCategoryModel;
+}
