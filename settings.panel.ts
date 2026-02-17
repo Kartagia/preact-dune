0d768f715dd3aed08b32faaf3148f8342f44d7dc
Kartagia <antti@kautiainen.com>
2026-02-17 13:54:13 +0200

    doc: createEntries
    Fixing creation of an item entreis
    Changes to be committed:
    	modified:   settings.panel.ts
        - doc: function createEntries
        - fix: component SettingsPanel using controller for getting values
        of entries.
            - fix: callback handleChange properly handle single item entry
                change.
            - fix: invalid HTML parameter calss to class.
        - feat: component SettingsPanel
            - feat: style class "setting" for div of the settings panel.
            - feat: button save mechanics.
            - feat: button default mechanics.
            - feat: button cancel mechanics

--- settings.panel.ts
+++ settings.panel.ts
@@ -1,5 +1,5 @@
 import { html, useState } from 'preact';
-import { SettingEntryModel, SettingsModel, SettingsDescription, isSettingItemModel, isSettingCategoryModel } from "./settings.model";
+import { SettingEntryModel, SettingsModel, SettingsDescription, isSettingItemModel, isSettingCategoryModel, SettingItemModel, nameToLabel } from "./settings.model";
 import { SettingsCategory } from './settings.category';
 import SettingsItem from './settings.item';
 import { SettingsController } from './settings.control';
@@ -43,10 +43,17 @@ export interface SettingsPanelProps<T> {
     onsave?: (settings: SettingsModel) => void;
 }
 
+/**
+ * Create entries with values taken from the controller.
+ * @param settings The settings model.
+ * @param controller The optional session controller used to determine the value of the model entries.
+ * @returns The list of setting entry models for the settings with controller values for all entries controller
+ * recognizes. 
+ */
 export function createEntries<T>(settings: SettingsModel, controller?: SettingsController<T>): SettingEntryModel[] {
     if (controller) {
         // Adding the values of the settings to the entries. 
-        return (settings.entries ?? []).map( 
+        return (settings.entries ?? []).map(
             (entry: SettingEntryModel) => {
                 if (isSettingCategoryModel(entry)) {
                     return controller.setCategoryValue(entry, entry.prefix);
@@ -69,24 +76,49 @@ export function createEntries<T>(settings: SettingsModel, controller?: SettingsC
 export function SettingsPanel<T>(props: SettingsPanelProps<T>) {
     const [settings, setSettings] = useState<SettingsModel>(props.model);
     const [currentSettings, setCurrentSettings] = useState<SettingsModel>(settings);
+    const [entries, setEntries] = useState<Array<SettingEntryModel>>(createEntries(currentSettings, props.controller));
     const [saved, setSaved] = useState<boolean>(true);
     const [unchanged, setUnchanged] = useState<boolean>(true);
 
-    const handleChange = (property: string, value: string) => {
+    const handleChange = (property: string, value?: string) => {
         const propertyChain = property.split(".");
-        setSettings( (current: SettingsModel) => {
-
+        setSettings((current: SettingsModel) => {
             var newSettings = current;
-            if (propertyChain.length > 1) {
+            if (props.controller) {
+                // Performing controller change. 
+                
+            } else if (propertyChain.length > 1) {
                 // Categories.
                 console.warn("Updating category not yet supported");
                 return current;
             } else {
-                // Items.
-                if (!Object.is(value,current.items[property].value)) {
+                // Items update without controller.
+                const propertyName = propertyChain[0];
+                const propertyEntry = current.items?.find((entry: SettingEntryModel) => ("name" in entry && entry.name === propertyName));
+                if (propertyEntry === undefined || !Object.is(value, propertyEntry?.value)) {
                     // The value has changed.
-                    const propertyEntry = current.items[propertyChain[0]];
-                    newSettings = {...current, items: {...current.items, [propertyChain[0]]: {...propertyEntry, value}}};
+                    if (value === undefined) {
+                        // Removing the entry.
+                        if (propertyEntry)
+                            newSettings = { ...current, entries: current.entries.filter(entry => !("name" in entry && entry.name === propertyName)) }
+                    } else {
+                        // Replacing the entry. 
+                        if (propertyEntry) {
+                            // Updating existing.
+                            newSettings = {
+                                ...current, entries: current.entries.map(entry => ("name" in entry && entry.name === propertyName) ? {
+                                    ...entry,
+                                    value
+                                } : entry)
+                            };
+                        } else {
+                            // Adding new entry.
+                            newSettings = {
+                                ...current, 
+                                entries: [...current.entries, {name: propertyName, label: nameToLabel(propertyName), value}]
+                            };
+                        }
+                    }
                 }
             }
             if (Object.is(newSettings, current)) {
@@ -96,7 +128,7 @@ export function SettingsPanel<T>(props: SettingsPanelProps<T>) {
             setSaved(true);
             setUnchanged(true);
             return newSettings;
-        });          
+        });
     };
 
     const handleSave = (event: MouseEvent) => {
@@ -123,20 +155,19 @@ export function SettingsPanel<T>(props: SettingsPanelProps<T>) {
         props.onclose?.();
     }
 
-    return html`<div>
+    return html`<div class="settings">
     <div class="header">${props.title ?? "Settings"}</div>
-    <div class="main">${
-        settings.entries.map( (entry: SettingEntryModel) => {
-            if ("entries" in entry) {
-                // Category entry.
-                return html`<${SettingsCategory} model="${entry}" onchange="${handleChange}" />`;
-            } else {
-                // Item entry.
-                return html`<${SettingsItem} model="${entry}" onchange="${handleChange}" />`;
-            }
-        })
-    }</div>
-    <div calss="footer">
+    <div class="main">${settings.entries.map((entry: SettingEntryModel) => {
+        if ("entries" in entry) {
+            // Category entry.
+            return html`<${SettingsCategory} model="${entry}" onchange="${handleChange}" />`;
+        } else {
+            // Item entry.
+            return html`<${SettingsItem} model="${entry}" onchange="${handleChange}" />`;
+        }
+    })
+        }</div>
+    <div class="footer">
         <button type="submit" onclick="${handleSave}" disabled="${saved}" value="save">Save</button>
         <button type="default" onclick="${handleDefault}" disabled="${unchanged}" value="revert">Revert</button>
         <button type="cancel" onclick="${handleCancel}" >Close</button>
